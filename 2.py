d4fcb03d036792fb0ff899dc8d6ae155347fa6e1
alkulesh <a.kulesh@internet.ru>
2023-11-11 17:42:02 +0300

    Выполнила 2

---
+++ 2.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+dataset = pd.read_csv('test.csv')
+values = dataset.sample(n=1000)
+missing = values.isnull().sum()
+print("Пропущенные значения:\n", missing)
+
+fig, axes = plt.subplots(1,2, figsize=(10,5))
+values['Id'].plot.box(ax=axes[0])
+values['Id'].plot.hist(ax=axes[1])
+plt.savefig('plot2.png')
+
+
+values['Id'].fillna(values['Id'].mean(), inplace=True)
+lower_bound = values['Id'].quantile(0.05)
+upper_bound = values['Id'].quantile(0.95)
+values['Id']= np.where(values['Id'] < lower_bound, lower_bound, values['Id'])
+values['Id'] = np.where(values['Id'] > upper_bound, upper_bound, values['Id'])
+
+room_counts = values['Rooms'].value_counts()
+print("Количество квартир по количеству комнат: ", room_counts)
+
+pivot_table = values.pivot_table(index='DistrictId', columns='Rooms', values='Square',aggfunc="sum", fill_value=0)
+if 'DistrictId' in values.columns and 'Rooms' in values.columns:
+    pivot_table = values.pivot_table(index='DistrictId', columns='Rooms', values='Square',aggfunc="sum", fill_value=0)
+else:
+    print("Столбцы 'DistrictId' и/или 'Rooms' отсутстыуют")
+print("Сводная таблица:\n ", pivot_table)
+
+plt.savefig('plot2.png')
+
